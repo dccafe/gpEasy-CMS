0180430200f6ae0224c7219484bb3d7cdcb45401
Daniel Café <dccafe@gmail.com>
2013-10-27 00:23:24 +0200

    using generic config

--- include/tool/ldap.php
+++ include/tool/ldap.php
@@ -6,7 +6,7 @@ $ldaphost = "localhost";        // LDAP server
 $ldapport = 389;                // Default Port
 $ldapadmin= 'admin_user';       // Admin user (like: 'cn=admin,dc=domain,dc=com')
 $ldappasswd='admin_password';   // Admin password
-$ldapgroup  = "users_group";    // Set the base tree to look for users (Normally 'ou=People,dc=adss,dc=fr')
+$ldapgroup  = "users_group";    // Set the base tree to look for users (Normally 'ou=People,dc=domain,dc=com')
 $ldapfilter = "(cn=*)";         // Set the search filter
 $ldapfields = array("cn","uid","userpassword","mail");  // Choose what you wish to get from the LDAP database
 
