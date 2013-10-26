c08748f247bf99e25a53dce360b9ab05444cf78b
Daniel Café <dccafe@gmail.com>
2013-10-26 20:08:35 +0200

    adding LDAP configuration file

---
+++ include/tool/ldap.php
@@ -0,0 +1,12 @@
+<?php
+defined('is_running') or die('Not an entry point...');
+
+$ldap_enable = true;            // Enable LDAP user authentication
+$ldaphost = "localhost";        // LDAP server
+$ldapport = 389;                // Default Port
+$ldapadmin= 'admin_user';       // Admin user (like: 'cn=admin,dc=domain,dc=com')
+$ldappasswd='admin_password';   // Admin password
+$ldapgroup  = "users_group";    // Set the base tree to look for users (Normally 'ou=People,dc=adss,dc=fr')
+$ldapfilter = "(cn=*)";         // Set the search filter
+$ldapfields = array("cn","uid","userpassword","mail");  // Choose what you wish to get from the LDAP database
+
